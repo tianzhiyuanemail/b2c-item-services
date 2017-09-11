aaf0dff05c0f39482ed9104417ca8c3c93b1b1f7
yuan <tianzhiyuanemail@163.com>
2017-09-11 23:27:14 +0800

    Initial commit

---
+++ b2c-item-service-web/src/main/java/com/b2c/tests/Test.java
@@ -0,0 +1,34 @@
+package com.b2c.tests;
+
+import lombok.extern.slf4j.Slf4j;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import com.b2c.IStudentService;
+
+/**
+ * Created by Administrator on 2017/7/15 0015.
+ */
+
+@RestController
+@Slf4j
+public class Test {
+
+    @Autowired
+    private IStudentService studentService;
+    private Logger log = LoggerFactory.getLogger(Test.class);
+
+    @RequestMapping("/aaa")
+    public Student select(String name){
+
+        Student student = studentService.selectByName(name);
+
+        log.info("ssssssssssssssssssssssssssssssssssssssssssssssssss");
+        log.error("wwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww");
+        return student;
+    }
+
+
+}
